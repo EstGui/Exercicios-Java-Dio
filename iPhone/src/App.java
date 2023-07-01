ba4e850f7fee35914e6ad772c74ee3e52ef42453
Guilherme Esteves <115667593+EstGui@users.noreply.github.com>
2023-07-01 17:28:30 -0300

    commit Inicial

---
+++ iPhone/src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        iPhone iphone = new iPhone();
+
+        iphone.tocar();
+        iphone.pausar();
+        iphone.selecionarMusica();
+        iphone.ligar();
+        iphone.atender();
+        iphone.iniciarCorreioVoz();
+        iphone.exibirPagina();
+        iphone.adicionarNovaAba();
+        iphone.atualizarPagina();
+    }
+}
---
+++ iPhone/src/iPhone.java
@@ -0,0 +1,38 @@
+public class iPhone implements ReprodutorMusical, AparelhoTelefônico, NavegadorInternet{
+    public void tocar(){
+        System.out.println("TOCANDO MÚSICA");
+    };
+
+    public void pausar(){
+        System.out.println("PAUSANDO MÚSICA");
+    };
+
+    public void selecionarMusica(){
+        System.out.println("SELECIONANDO MÚSICA");
+    };
+
+    public void ligar(){
+        System.out.println("LIGANDO");
+    };
+
+    public void atender(){
+        System.out.println("ATENDENDO");
+    };
+
+    public void iniciarCorreioVoz(){
+        System.out.println("INICIANDO CORREIO DE VOZ");
+    };
+
+    public void exibirPagina(){
+        System.out.println("EXIBINDO PÁGINA");
+    };
+
+    public void adicionarNovaAba(){
+        System.out.println("ADICIONANDO NOVA ABA");
+    };
+
+    public void atualizarPagina(){
+        System.out.println("ATUALIZANDO PÁGINA");
+    };
+
+}
